e54fbadcbaf1e025f7142ac1b12bc9160ed3b8be
jotapec <jpdecarvalho3001@gmail.com>
2017-11-20 23:53:09 -0200

    relatorio v1

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using SalaoIedaV4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalaoIedaV4.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report   03    var vendas = db.PedidoCabecalhos.Where(i => i.DataPedido >= dataInicial && i.DataPedido <= dataFinal).OrderBy(p=>p.PedidoCabId).ToList<PedidoCabecalho>();
+        public class AgendaTipo
+        {
+            public string NomeTipoServico { get; set; }
+            public int agendas { get; set; }
+        }
+        private Context dc= new Context();
+        public ActionResult Index()
+        {
+            ViewBag.ClienteCount = dc.Clientes.Select(Cliente => Cliente.idCliente).Count();
+            ViewBag.ServicoCount = dc.Servicos.Select(Servico => Servico.idServico).Count();
+            ViewBag.AgendaMesCount = dc.Agendas.Where(a => a.dt_data_agendada.Month == DateTime.Now.Month && a.cancelamento == false).Count();
+            ViewBag.AgendaTotalCount = dc.Agendas.Where(a => a.cancelamento == false).Count();
+            ViewBag.AgendaMesTipoCount = dc.Agendas.Where(a => a.dt_data_agendada.Month == DateTime.Now.Month && a.cancelamento == false).GroupBy(a => a.Tipo_Servico.desc_tipo_servico).Select(x => new AgendaTipo {NomeTipoServico = x.Key, agendas = x.Count()} ).ToList();
+            ViewBag.AgendaTotalTipoCount = dc.Agendas.GroupBy(a => a.Tipo_Servico.desc_tipo_servico).Select(x => new AgendaTipo { NomeTipoServico = x.Key, agendas = x.Count() }).ToList();
+            return View();
+        }
+        
+        
+    }
+}
